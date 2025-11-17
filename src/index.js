439b578dddc8477245629efa67a2159d49948609
nikohonkanen <honkanen.niko@gmail.com>
2025-11-17 18:22:09 +0200

    Firebasen alustus projektiin.

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
+//import { initializeApp } from 'firebase/app';
+
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
+//import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCUZNqdanUH2Z63t5GWw1JjY-0ffwqCy7I",
+
+  authDomain: "tuotekehitysprojekti-5f330.firebaseapp.com",
+
+  projectId: "tuotekehitysprojekti-5f330",
+
+  storageBucket: "tuotekehitysprojekti-5f330.firebasestorage.app",
+
+  messagingSenderId: "362924183192",
+
+  appId: "1:362924183192:web:337b854b2ecc8b53e48aed"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        console.log("Logged in!");
+        
+    } else {
+        console.log("No user");
+        
+    }
+});
