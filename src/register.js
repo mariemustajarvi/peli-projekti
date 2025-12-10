1e91b2108c50db5ed391e8607472cffaf5990a0f
nikohonkanen <honkanen.niko@gmail.com>
2025-12-10 19:29:48 +0200

    Lisätty mahdollisuus valita agenttinimi

--- src/register.js
+++ src/register.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
-//import { initializeApp } from 'firebase/app';
 
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
-//import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js';
 
 const firebaseConfig = {
 
@@ -16,37 +16,57 @@ const firebaseConfig = {
 
     messagingSenderId: "362924183192",
 
-    appId: "1:362924183192:web:337b854b2ecc8b53e48aed"
+    appId: "1:362924183192:web:337b854b2ecc8b53e48aed",
+
+    databaseURL: "https://tuotekehitysprojekti-5f330-default-rtdb.europe-west1.firebasedatabase.app"
 
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Tarkistetaan onko käyttäjä jo kirjautunut sisälle, jos on niin siirrytään etusivulle.
+/* Tarkistetaan onko käyttäjä jo kirjautunut sisälle, jos on niin siirrytään etusivulle.
 onAuthStateChanged(auth, (user) => {
     if (user) {
         window.location.href = 'index.html'
     }
-});
+});*/
+
+let writeUserData = (userId, name) => {
+    const db = getDatabase();
+    const reference = ref(db, 'users/' + userId)
+
+    set(reference, {
+        username: name
+    }).then(() => {
+        console.log("lisätty");
+        window.location.href = 'index.html'
+    })
+}
 
 const form = document.getElementById('loginform');
 
 form.addEventListener('submit', event => {
     event.preventDefault()
+    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if (password.length < 6) {
-        console.log("Liian lyhyt salasana");
+    if (password.length < 6 || username.length > 20) {
+        console.log("Liian lyhyt salasana tai liian pitkä agenttinimi");
     } else {
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
+            .then((userCredential) => {
+                const userId = userCredential.user.uid;
+                console.log(userId);
+                console.log(username);
+                writeUserData(userId, username)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+
+            });
     }
 });
