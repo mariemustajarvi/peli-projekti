72a84caf72803c14082f8692abc1ae5c68833455
nikohonkanen <honkanen.niko@gmail.com>
2025-12-13 17:44:29 +0200

    Rekisteröinti toimii

--- js/index.js
+++ js/index.js
@@ -1,4 +1,58 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
 
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
+
+import { getDatabase, ref, child, get } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCUZNqdanUH2Z63t5GWw1JjY-0ffwqCy7I",
+
+  authDomain: "tuotekehitysprojekti-5f330.firebaseapp.com",
+
+  projectId: "tuotekehitysprojekti-5f330",
+
+  storageBucket: "tuotekehitysprojekti-5f330.firebasestorage.app",
+
+  messagingSenderId: "362924183192",
+
+  appId: "1:362924183192:web:337b854b2ecc8b53e48aed",
+
+  databaseURL: "https://tuotekehitysprojekti-5f330-default-rtdb.europe-west1.firebasedatabase.app"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+
+const loginBtn = document.getElementById('logInOutBtnLink')
+const loginBtnText = document.getElementById('logInOutBtnText')
+
+let loggedIn = false; // Tätä käytetään vain tarkistamaan tarvitseeko käyttäjää kirjata ulos nappia painettaessa!
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    loggedIn = true;
+    loginBtnText.innerHTML = 'Kirjaudu ulos';
+  } else {
+    loggedIn = false;
+    loginBtnText.innerHTML = "Kirjaudu sisään / Rekisteröidy"
+  }
+});
+
+loginBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  if (loggedIn) {
+    signOut(auth).then(() => {
+      loginBtnText.innerHTML = "Kirjaudu sisään / Rekisteröidy"
+    })
+  } else {
+    window.location.href = loginBtn.href;
+  }
+});
+
+/* Vanhat
 // Käyttää localStoragea (myöhemmin voidaan vaihtaa Firebaseen).
 
 (function () {
@@ -119,3 +173,4 @@
 
   document.addEventListener("DOMContentLoaded", setupIndexPage);
 })();
+*/
