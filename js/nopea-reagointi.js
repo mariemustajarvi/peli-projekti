37d023a1be01ec77f5701aa3cff0269920009c1f
nikohonkanen <honkanen.niko@gmail.com>
2025-12-13 20:10:09 +0200

    Pelin voi pelata nyt alusta loppuun.

--- js/nopea-reagointi.js
+++ js/nopea-reagointi.js
@@ -1,3 +1,50 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
+
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
+
+import { getDatabase, ref, update } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCUZNqdanUH2Z63t5GWw1JjY-0ffwqCy7I",
+
+  authDomain: "tuotekehitysprojekti-5f330.firebaseapp.com",
+
+  projectId: "tuotekehitysprojekti-5f330",
+
+  storageBucket: "tuotekehitysprojekti-5f330.firebasestorage.app",
+
+  messagingSenderId: "362924183192",
+
+  appId: "1:362924183192:web:337b854b2ecc8b53e48aed",
+
+  databaseURL: "https://tuotekehitysprojekti-5f330-default-rtdb.europe-west1.firebasedatabase.app"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+let userRef = null;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const userId = user.uid;
+    userRef = ref(db, 'users/' + userId);
+  } else {
+    window.location.href = "index.html";
+  }
+});
+
+const savePoints = (points) => {
+  if(userRef) {
+    const updates = {};
+    updates['/scores/8/'] = points;
+    updates['completedMissions/8/'] = true;
+    update(userRef, updates)
+  }
+};
+
 const allQuestions = [
   {
     text: 'On ok klikata "Hyväksy kaikki evästeet" jokaisella verkkosivustolla',
@@ -282,6 +329,8 @@ function finishGame() {
 const maxPoints = 250;
 const pointsPerCorrect = Math.floor(maxPoints / roundQuestions.length); // 25
 let points = correctCount * pointsPerCorrect;
+
+savePoints(points);
 
 // 250 pistettä
 if (correctCount === roundQuestions.length) points = maxPoints;
